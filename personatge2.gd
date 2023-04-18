68a11291fe53b98318b457be375b73cb45b62d78
saragonza <127503764+saragonza@users.noreply.github.com>
2023-04-18 09:54:23 +0200

    personatge 2 fet i comnça a cuadrar amb tilemap

---
+++ personatge2.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var velocitat := 200
+var moviment := Vector2.ZERO
+var gravetat := Vector2.DOWN * 900
+var salt :=Vector2.UP * 500
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _process(delta):
+	moviment += gravetat*delta
+	moviment.x = velocitat
+	if Input.is_action_just_pressed("ves_amunt") and is_on_floor():
+		moviment += salt
+		
+	
+	moviment = move_and_slide(moviment, Vector2.UP)
+	
+	anima(velocitat)
+	
+func anima(velocitat):
+	$AnimatedSprite.play("walk")
+	$AnimatedSprite.flip_h = false
